0cd1adf0f795c4a469053874b25d84dae27999ab
Roberto <roberto@MacBook-Pro-di-Roberto.local>
2017-09-06 22:39:17 +0200

    fixed lein repl and lein run commands

--- project.clj
+++ project.clj
@@ -3,12 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "3.6.1"]
                  [clj-jgit "0.8.9"]
                  [ring/ring-core "1.5.0"]
                  [http-kit "2.2.0"]
                  ]
+  :plugins [[cider/cider-nrepl "0.15.0"]]  
   :main ^:skip-aot cdflow.core
+  :aot :all
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
